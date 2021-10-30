4448d264a9dc093e656780d8848106f03093ab95
Sergei Anciferov <sergozas@yandex.ru>
2021-10-31 00:43:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/task2/Activity2.kt
@@ -0,0 +1,51 @@
+package com.example.task2
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.example.task2.databinding.Activity2Binding
+
+class Activity2 : AppCompatActivity() {
+
+    private lateinit var binding: Activity2Binding
+
+    companion object {
+        const val FROM_THIRD = 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = Activity2Binding.inflate(layoutInflater)
+        binding.toFirst.setOnClickListener { toFirstClick() }
+        binding.toThird.setOnClickListener { toThirdClick() }
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { onAboutClick(it) }
+        setContentView(binding.root)
+    }
+
+    private fun toFirstClick(){
+        finish()
+    }
+
+    private fun toThirdClick(){
+        startActivityForResult(Intent(this, Activity3::class.java), FROM_THIRD)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FROM_THIRD && resultCode == Activity.RESULT_OK) {
+            finish()
+        }
+    }
+
+    private fun onAboutClick(menuItem: MenuItem) : Boolean {
+        when (menuItem.itemId) {
+            R.id.to_about -> {
+                startActivity(Intent(this, ActivityAbout::class.java))
+            }
+        }
+        return false
+    }
+
+}
---
+++ app/src/main/java/com/example/task2/Activity3.kt
@@ -0,0 +1,39 @@
+package com.example.task2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.example.task2.databinding.Activity3Binding
+
+class Activity3 : AppCompatActivity() {
+
+    private lateinit var binding: Activity3Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = Activity3Binding.inflate(layoutInflater)
+        binding.toFirst.setOnClickListener { toFirstClick() }
+        binding.toSecond.setOnClickListener { toSecondClick() }
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { onAboutClick(it) }
+        setContentView(binding.root)
+    }
+
+    private fun toFirstClick() {
+        this.setResult(RESULT_OK)
+        finish()
+    }
+
+    private fun toSecondClick() {
+        finish()
+    }
+
+    private fun onAboutClick(menuItem: MenuItem) : Boolean {
+        when (menuItem.itemId) {
+            R.id.to_about -> {
+                startActivity(Intent(this, ActivityAbout::class.java))
+            }
+        }
+        return false
+    }
+}
